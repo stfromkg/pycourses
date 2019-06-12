25062890596008c7254225394cdd2e772f4cec63
stfromkg <51327276+stfromkg@users.noreply.github.com>
2019-06-12 15:11:07 +0600

    Add files via upload

---
+++ 1006 task3.py
@@ -0,0 +1,14 @@
+a = list()
+n = int(input("колличество цифр:"))
+for i in range(n):
+    b = int(input())
+    a.append(b)
+cnt1 = 0
+cnt2 = 0
+cnt3 = 0
+if n < 0 cnt +1 :
+
+
+#len(a)
+#print(len(a))
+
