691687a0e4efc9bd412b5165dccf8eac36a6be52
stfromkg <51327276+stfromkg@users.noreply.github.com>
2019-06-12 15:11:47 +0600

    Add files via upload

---
+++ 1006task8 (35,87).py
@@ -0,0 +1,7 @@
+for i in range(35, 88):
+   if i % 7 == 1:
+       print("для остатка 1: " + str(i))
+   elif i % 7 == 2:
+       print("для остатка 2: " + str(i))
+   elif i % 7 == 5:
+       print("для остатка 5: " + str(i))
