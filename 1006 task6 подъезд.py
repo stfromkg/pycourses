4e8468c6a96e641cdbee85c94a2674a8b142bdf5
stfromkg <51327276+stfromkg@users.noreply.github.com>
2019-06-12 15:11:23 +0600

    Add files via upload

---
+++ 1006 task6 подъезд.py
@@ -0,0 +1,11 @@
+for n in range(1,90):
+    n = int(input())
+    if n <= 20:
+        print(1)
+    elif n <= 48:
+        print(2)
+    elif n <= 90:
+        print(3)
+    else:
+        print(str("Not found"))
+
