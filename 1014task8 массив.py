99216412edea3e75e3a251c36050d429038735b9
stfromkg <51327276+stfromkg@users.noreply.github.com>
2019-06-19 08:26:56 +0600

    Add files via upload

---
+++ 1014task8 массив.py
@@ -0,0 +1,12 @@
+import re
+
+1# Дан массив:
+# [3 4 1 2 5]
+# Превратить его в массив
+# [hello3 hello 4 hello1 hello2 hello5]
+a = [3 4 1 2 5]
+b = (str(len(a)))
+print(b)
+
+#b.replace(' ','hello')
+#print(b)
